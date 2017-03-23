fcbecac9b77672d89abe3f424c9c334f80dc9398
Ratchakran Prapanon <beelzerbub@hotmail.com>
2017-03-22 20:56:23 -0700

    fix delete Jquery

--- login.php
+++ login.php
@@ -187,15 +187,6 @@
 				$("#login-form").delay(100).fadeIn(100);
 				$("#register-form").fadeOut(100);
 				$("#forgot-form").fadeOut(100);
-				$('#register-form-link').removeClass('active');
-				$(this).addClass('active');
-				e.preventDefault();
-			});
-			$('#register-form-link').click(function(e) {
-				$("#register-form").delay(100).fadeIn(100);
-				$("#login-form").fadeOut(100);
-				$("#forgot-form").fadeOut(100);
-				$('#login-form-link').removeClass('active');
 				$(this).addClass('active');
 				e.preventDefault();
 			});
@@ -203,14 +194,12 @@
 				$("#forgot-form").delay(100).fadeIn(100);
 				$("#forgot-header").delay(100).fadeIn(100);
 				$("#login-form-link").fadeOut(100);
-				$("#register-form-link").fadeOut(100);
 				$("#login-form").fadeOut(100);
 				$('#login-form-link').removeClass('active');
 				e.preventDefault();
 			});
 			$('#backBtn').click(function(e) {
 				$("#login-form-link").delay(100).fadeIn(100);
-				$("#register-form-link").delay(100).fadeIn(100);
 				$("#login-form").delay(100).fadeIn(100);
 				$("#forgot-form").fadeOut(100);
 				$("#forgot-header").fadeOut(100);
