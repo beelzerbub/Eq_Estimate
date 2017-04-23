f8e97d392a602c546dcfc1b3a4d82b1a1cce5d2d
Ratchakran Prapanon <beelzerbub@hotmail.com>
2017-04-23 07:47:40 +0700

    fix noticed bug forgot about =

--- assets/template/notice.php
+++ assets/template/notice.php
@@ -54,7 +54,7 @@ if ($action ==  "forgot_fail") {
 		</button>
 	</div>
 	<?php
-} else if ($action = "teacher_delete_wrning") {
+} else if ($action == "teacher_delete_warning") {
 	?>
 	<div class="alert alert-danger" role="alert">
 		เนื่องจากครูประจำชั้นดังกล่าวมีประวัติการประเมิน จึงไม่สามารถลบครูดังกล่าวได้ แต่ระบบจะทำการล็อคการใช้งานของครูดังกล่าวแทน
